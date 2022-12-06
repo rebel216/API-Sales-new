f34194a66f1d4c2ca9e05bd9cf3e758c049da1ac
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-06 19:44:26 +0530

    Getting file list and file with id number

--- db/migrate/20221203131624_create_items.rb
+++ db/migrate/20221203131624_create_items.rb
@@ -3,8 +3,6 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "file"
-    t.timestamps
     end
   end
 end
