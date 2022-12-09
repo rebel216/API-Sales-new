07d23f716480dfcab6be082b1796c9ef23e08a6d
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-09 06:40:05 +0530

    Data tables fixed

--- db/migrate/20211205015619_create__items.rb
+++ db/migrate/20211205015619_create__items.rb
@@ -1,6 +1,6 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
-    create_table :Items do |t|
+    create_table :items do |t|
       t.string :name
       t.string :description
       
